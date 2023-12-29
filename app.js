6db1c6a7a357e6a6dd37feb967508911bb40aa47
Hasan Çoban <hasancoban13579@gmail.com>
2023-12-30 01:16:35 +0300

    Display the board on the screen

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
 let round = 1;
 
+const ScreenManager = (function() {
+    const domCells = document.querySelectorAll('.cell');
+
+    for(let i = 0; i < domCells.length; i++){
+        let row = Math.floor(i / 3) + 1;
+        let column = i % 3 + 1;
+
+        domCells[i].addEventListener('click', () => {
+            console.log(`clicked ${row} ${column}`);  
+            GameController.play(row, column);
+        })
+    }
+
+    const displayValues = () => {
+        for (let i = 0; i < GameBoard.getRowSize(); i++) {
+            for (let j = 0; j < GameBoard.getColumnSize(); j++) {
+                let index = i * 3 + j;
+                domCells[index].textContent = GameBoard.getCell(i, j).getValue();
+            }
+        }
+    }
+
+    const declareResult = (result) => {
+        const resultText = document.querySelector('.result-text');
+        resultText.textContent = result;
+    }
+
+    return {
+        displayValues,
+        declareResult
+    }
+}
+)();
+
+
 const GameBoard = (function() {
     const rows = 3;
     const columns = 3;
@@ -16,20 +51,24 @@ const GameBoard = (function() {
     const showValues = () => {
         board.forEach(element => {
             console.log(element[0].getValue(), element[1].getValue(), element[2].getValue());
-            /*element.forEach(cell => {
-                console.log("")
-                console.log(cell.getValue());
-            })*/
         });
     }
 
+    const getRowSize = () => rows;
+    const getColumnSize = () => columns;
+
+    const getCell = (row, column) => board[row][column];
+
     return {
         board,
-        showValues
+        showValues,
+        getRowSize,
+        getColumnSize,
+        getCell
     }
 })();
 
-function Cell(sign = "-") {
+function Cell(sign = "") {
     let value = sign;
 
     const getValue = () => value;
@@ -70,16 +109,19 @@ const GameController = (function() {
                 GameBoard.board[row-1][column-1].changeValue(activePlayer.getSign());
                 GameBoard.showValues();
                 activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+                ScreenManager.displayValues();
                 checkWinCondition();
             }
 
             else {
                 console.log("This cell is already occupied! Try Again.");
+                ScreenManager.declareResult("This cell is already occupied! Try Again.");
             }
         }
         
         else {
             console.log("You can't play! Game is already over!");
+            ScreenManager.declareResult("You can't play! Game is already over!");
         }
 
 
@@ -107,22 +149,30 @@ const GameController = (function() {
             isOver = true;
             winner = GameBoard.board[0][0].getValue() === "X" ? playerOne : playerTwo;
             }
+        else if ((GameBoard.board[0][2].getValue() === "X" || GameBoard.board[0][2].getValue() === "O")
+             && GameBoard.board[0][2].getValue() === GameBoard.board[1][1].getValue() 
+                && GameBoard.board[0][2].getValue() === GameBoard.board[2][0].getValue()) {
+            isOver = true;
+            winner = GameBoard.board[0][2].getValue() === "X" ? playerOne : playerTwo;
+            }
 
         //check vertically    
-        if (GameBoard.board.every(row => row[0].getValue() === GameBoard.board[0][0].getValue() && GameBoard.board[0][0].getValue() !== "-")){
+        if (GameBoard.board.every(row => row[0].getValue() === GameBoard.board[0][0].getValue() && GameBoard.board[0][0].getValue() !== "")){
             isOver = true;
             winner = GameBoard.board[0][0].getValue() === "X" ? playerOne : playerTwo;
         }
-        else if (GameBoard.board.every(row => row[1].getValue() === GameBoard.board[0][1].getValue() && GameBoard.board[0][1].getValue() !== "-")){
+        else if (GameBoard.board.every(row => row[1].getValue() === GameBoard.board[0][1].getValue() && GameBoard.board[0][1].getValue() !== "")){
             isOver = true;
             winner = GameBoard.board[0][1].getValue() === "X" ? playerOne : playerTwo;
         }
-        else if (GameBoard.board.every(row => row[2].getValue() === GameBoard.board[0][2].getValue() && GameBoard.board[0][2].getValue() !== "-")){
+        else if (GameBoard.board.every(row => row[2].getValue() === GameBoard.board[0][2].getValue() && GameBoard.board[0][2].getValue() !== "")){
             isOver = true;
             winner = GameBoard.board[0][2].getValue() === "X" ? playerOne : playerTwo;
         }
 
-        if(isOver) console.log(`Game is over! Winner is ${winner.getName()}`);
+        if(isOver) console.log(`Oyun bitti! Kazanan ${winner.getName()}!`);
+
+        if(isOver) ScreenManager.declareResult(`Oyun bitti! Kazanan ${winner.getName()}!`);
     } 
 
     return {
@@ -130,10 +180,3 @@ const GameController = (function() {
         checkWinCondition
     };
 })();
-
-GameController.play(1,1);
-GameController.play(1,2);
-GameController.play(3,1);
-GameController.play(3,3);
-GameController.play(2,1);
-GameController.play(3,2);
