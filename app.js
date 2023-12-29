161451195dabc476398067389adef93638c63ab1
Hasan Çoban <hasancoban13579@gmail.com>
2023-12-29 15:06:49 +0300

    Add win condition checks

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let round = 1;
+
 const GameBoard = (function() {
     const rows = 3;
     const columns = 3;
@@ -56,25 +58,82 @@ function Player(name, sign) {
 const GameController = (function() {
     const playerOne = Player("Hasan", "X");
     const playerTwo = Player("Recep", "O");
+    let isOver;
 
     let activePlayer = playerOne;
 
     const play = (row, column) => {
-        console.log("Playing round 1");
+        console.log(`Playing round ${round++}. ${activePlayer.getName()}'s (${activePlayer.getSign()}) round:`);
+        if(!isOver){
+            
+            if (GameBoard.board[row-1][column-1].getValue() !== "X" && GameBoard.board[row-1][column-1].getValue() !== "O") {
+                GameBoard.board[row-1][column-1].changeValue(activePlayer.getSign());
+                GameBoard.showValues();
+                activePlayer = activePlayer === playerOne ? playerTwo : playerOne;
+                checkWinCondition();
+            }
 
-        GameBoard.board[row-1][column-1].changeValue(activePlayer.getSign());
-        GameBoard.showValues();
+            else {
+                console.log("This cell is already occupied! Try Again.");
+            }
+        }
+        
+        else {
+            console.log("You can't play! Game is already over!");
+        }
+
+
     }
 
+    const checkWinCondition = () => {
+        isOver = false;
+        let winner;
+
+
+        //check horizontally
+        GameBoard.board.forEach(element => {
+            if ((element[0].getValue() === "X" || element[0].getValue() === "O")
+                 && element[0].getValue() === element[1].getValue()
+                  && element[0].getValue() === element[2].getValue()){
+                isOver = true;
+                winner = element[0].getValue() === "X" ? playerOne : playerTwo;
+            }
+        })
+
+        //check diagonally
+        if ((GameBoard.board[0][0].getValue() === "X" || GameBoard.board[0][0].getValue() === "O")
+             && GameBoard.board[0][0].getValue() === GameBoard.board[1][1].getValue() 
+                && GameBoard.board[0][0].getValue() === GameBoard.board[2][2].getValue()) {
+            isOver = true;
+            winner = GameBoard.board[0][0].getValue() === "X" ? playerOne : playerTwo;
+            }
+
+        //check vertically    
+        if (GameBoard.board.every(row => row[0].getValue() === GameBoard.board[0][0].getValue() && GameBoard.board[0][0].getValue() !== "-")){
+            isOver = true;
+            winner = GameBoard.board[0][0].getValue() === "X" ? playerOne : playerTwo;
+        }
+        else if (GameBoard.board.every(row => row[1].getValue() === GameBoard.board[0][1].getValue() && GameBoard.board[0][1].getValue() !== "-")){
+            isOver = true;
+            winner = GameBoard.board[0][1].getValue() === "X" ? playerOne : playerTwo;
+        }
+        else if (GameBoard.board.every(row => row[2].getValue() === GameBoard.board[0][2].getValue() && GameBoard.board[0][2].getValue() !== "-")){
+            isOver = true;
+            winner = GameBoard.board[0][2].getValue() === "X" ? playerOne : playerTwo;
+        }
+
+        if(isOver) console.log(`Game is over! Winner is ${winner.getName()}`);
+    } 
+
     return {
-        play
+        play,
+        checkWinCondition
     };
 })();
 
 GameController.play(1,1);
-GameController.play(1,3);
-GameController.play(3,3);
-GameController.play(2,3);
 GameController.play(1,2);
+GameController.play(3,1);
+GameController.play(3,3);
+GameController.play(2,1);
 GameController.play(3,2);
-GameBoard.showValues();
